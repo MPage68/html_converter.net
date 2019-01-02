92ecaa358dcb114556c65e98586d14933c906658
mpage68 <mikepage68@live.com>
2019-01-02 13:11:23 -0500

    practice code C#

---
+++ html_converter/HTML.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace html_converter
+{
+    internal class HTML
+    {
+        string greeting;
+        string html;
+
+        public HTML()
+        {
+            greeting = "HTML Converter";
+            html = "<h1>Grocery List</h1>\n" +
+                "<ul>\n" +
+                "<li>Eggs</li>\n" +
+                "<li>Milk</li>\n" +
+                "<li>Butter</li>\n" +
+                "</ul>";
+        }
+
+
+
+        public string getGreeting()
+        {
+            Console.WriteLine("HTML Converter:\n");
+            return greeting;
+        }
+
+        public string getInput()
+        {
+            Console.WriteLine("INPUT:\n" + html);
+            return html;
+        }
+
+        public string getOutput()
+        {
+
+            Regex pattern = new Regex("[<>/hliu1]");
+            pattern.Replace(html, " ");
+
+            Console.WriteLine("OUTPUT:\n" + html);
+            return html;
+        }
+    }
+}
---
+++ html_converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace html_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HTML html = new HTML();
+
+            html.getGreeting();
+            html.getInput();
+            html.getOutput();
+            Console.ReadLine();
+
+        }
+    }
+}
